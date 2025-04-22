e649b20744085812f977358735ead1caef4244ae
take0913 <tphonex913@outlook.jp>
2025-04-22 18:31:50 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'take' && password == '0913'
+    end
+  end
 end
