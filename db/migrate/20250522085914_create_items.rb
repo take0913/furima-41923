ae541fd8d944b761d324f7b6c8343564aaf12b06
take0913 <tphonex913@outlook.jp>
2025-05-25 19:29:17 +0900

    カラム数の修正

--- db/migrate/20250522085914_create_items.rb
+++ db/migrate/20250522085914_create_items.rb
@@ -9,7 +9,8 @@ class CreateItems < ActiveRecord::Migration[7.1]
       t.integer :prefecture_id   null: false
       t.integer :delivery_day_id null: false
       t.integer :price           null: false
-      
+      t.integer :category_id     null: false
+
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
