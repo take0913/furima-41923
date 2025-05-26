e55a149aa201df7e8eafc4df410d087efb384efb
take0913 <tphonex913@outlook.jp>
2025-05-26 23:25:33 +0900

    商品詳細表示機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,6 @@ class Item < ApplicationRecord
   belongs_to :delivery_day
   has_one_attached :image
 
-
   validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true
