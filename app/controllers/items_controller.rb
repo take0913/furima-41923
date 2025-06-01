9dbf7849758b1713131cb481643ee5a50571042d
take0913 <tphonex913@outlook.jp>
2025-06-01 15:35:33 +0900

    商品削除機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -38,7 +38,6 @@ def update
 end
 
 def destroy
-  @item = Item.find(params[:id])
   return redirect_to root_path unless current_user == @item.user
 
   @item.destroy
