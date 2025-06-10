1d7b69d384050a224f4ef1f48a0f31b0e17e8c21
take0913 <tphonex913@outlook.jp>
2025-06-10 20:35:24 +0900

    商品購入機能の修正３

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -57,6 +57,12 @@ RSpec.describe OrderAddress, type: :model do
         expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
       end
 
+       it '電話番号が9桁以下だと保存できない' do 
+        @order_address.phone_number = '090123456'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+      end
+
       it '電話番号が12桁以上だと保存できない' do
         @order_address.phone_number = '090123456789'
         @order_address.valid?
